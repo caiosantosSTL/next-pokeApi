e9d38164b59354f386f98bd4479c8f0b74a8a896
Caio Santos da Silva <vorreicaio@hotmail.com>
2022-02-04 16:33:07 -0600

    cards

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,42 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import 'bootstrap/dist/css/bootstrap.css'
 
-export default function Home() {
+import styles from '../styles/Home.module.css'
+import Card from './components/Card'
+//import 'bootstrap/dist/css/bootstrap.css'
+
+export async function getStaticProps(params) {
+  const maxPoken = 50
+  const api = 'https://pokeapi.co/api/v2/pokemon/'
+
+  const res = await fetch(`${api}/?limit=${maxPoken}`)
+  const data = await res.json()
+
+  // add poken
+  data.results.forEach((item, index) => {
+    item.id = index + 1 // get the first id 1
+
+  })
+  return {
+    props: {
+      pokemons: data.results,
+    }
+  }
+}
+
+export default function Home({ pokemons }) {
   return (
-    <div className={styles.container}>
-        <h1>hsalkd</h1>
+    <div>
+      <div>
+        <div className={styles.title_container}>
+          <h1>Poke <span>Proj</span></h1> 
+        </div>
+
+        <div className={styles.poke_container}>
+          {pokemons.map((pokemon) => (
+            
+            <Card key={pokemon.id} pokemon={pokemon}/>
+          ))}
+        </div>
+      </div>
     </div>
   )
 }
