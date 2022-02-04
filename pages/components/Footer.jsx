d5fb44d50814d3d5e4661968e074a74819158eae
Caio Santos da Silva <vorreicaio@hotmail.com>
2022-02-03 22:20:31 -0600

    proj pokemon api

---
+++ pages/components/Footer.jsx
@@ -0,0 +1,11 @@
+import '../../styles/Footer.module.css'
+
+export default function Footer(params) {
+    return (
+        <footer>
+            <p>
+                <span>PokeProj</span>
+            </p>
+        </footer>
+    )
+}
---
+++ pages/components/Navbar.jsx
@@ -0,0 +1,21 @@
+import Link from "next/link";
+import styles from '../../styles/Navbar.module.css'
+
+export default function Navbar(){
+    return (
+        <nav className={styles.navbar}>
+            <div className={styles.logo}>
+                <p>Logo</p>
+                <h1>PokeProj</h1>
+            </div>
+            <ul className={styles.link_item}>
+                <li>
+                    <Link href={"/"}>Home</Link>
+                </li>
+                <li>
+                    <Link href={"/about"}>About</Link>
+                </li>
+            </ul>
+        </nav>
+    )
+}
